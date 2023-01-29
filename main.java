07857593f42865628a657e37e3ee6f69a56c1700
Cose31 <115685477+Cose31@users.noreply.github.com>
2023-01-29 16:50:24 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,38 @@
+
+
+
+public static void main()
+	{
+		//genericMatrix.read;
+		//genericMatrix.write;
+		int input = 2;
+		int input2 = 3;
+		int input3 = 4;  	//test for add matrix with different rows/columns
+		int input4 = 5;		//==
+		
+
+		Scanner scaninput = new Scanner(System.in);
+
+		System.out.println("Please enter what the dimensions of your matrix are (n rows, and m columns). Enter n then m: ");
+		String[] ln = sc.nextLine().split("");
+
+		input = ln[0];
+		input2 = ln[1];
+
+
+		genericMatrix<Integer> usermat1 = new genericMatrix<>;
+
+		//usermat1.genericRead(input, input2); 			//tests for genericRead
+		//usermat2.genericRead(input, input2);			//==
+		//usermat1.genericWrite();		//tests for genericWrite
+		//usermat2.genericWrite();		//==
+
+
+		//addSectiontest
+		//write comparison statement for matrixes rows and columns, if they do not match, then throw exception
+		
+		//genericAdd(usermat1, usermat2);
+
+		//sumSectionTest
+
+	}
